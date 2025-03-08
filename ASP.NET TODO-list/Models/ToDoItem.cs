72a305f4e31abf9481dd15d0aa60a156636db87b
PrayonFernando <prayonfernando@gmail.com>
2025-03-08 09:46:09 +0530

    Final Changes

--- ASP.NET TODO-list/Models/ToDoItem.cs
+++ ASP.NET TODO-list/Models/ToDoItem.cs
@@ -3,10 +3,10 @@
     public class ToDoItem
     {
         public int Id { get; set; }
-        public string? Title { get; set; } // Make nullable
-        public string? Description { get; set; } // Make nullable
+        public string? Title { get; set; } 
+        public string? Description { get; set; } 
         public bool IsCompleted { get; set; }
-        public string Priority { get; set; } = "Medium"; // Default value
-        public DateTime DueTime { get; set; } = DateTime.Now.AddDays(1); // Default value
+        public string Priority { get; set; } = "Medium"; 
+        public DateTime DueTime { get; set; } = DateTime.Now.AddDays(1); 
     }
 }
